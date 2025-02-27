f63458f5d81faf74de09b0d59cca9f9184de8aac
emtato <emiliama2006@gmail.com>
2025-02-27 12:24:36 -0500

    meoow

--- convert-api.py
+++ convert-api.py
@@ -1,2 +1,5 @@
-# Description:
+# Description: converts the api data to csv file, this file does not need to be run more than once on the full data file
 # Created by Emilia on 2025-02-27
+
+def get_ids(filename: str) -> list[int]:
+
