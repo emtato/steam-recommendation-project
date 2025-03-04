baeeeb2746b9d647c834ce0fe3c3e8ac0e8bd65d
emtato <emiliama2006@gmail.com>
2025-03-04 11:58:50 -0500

    i cooked

--- convert-api.py
+++ convert-api.py
@@ -1,10 +1,11 @@
 # Description: converts the api data to csv file, this file does not need to be run more than once on the full data file
 # Created by Emilia on 2025-02-27
-import json
+
 from objects import Game
 import requests
 
 apps = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json'
+exchange_rate = 1.45
 
 
 def get_ids(start: int, end: int) -> list[int]:
@@ -53,6 +54,10 @@ def get_clean_requirements(req):
     return ""
 
 
+def get_single_info(id: int) -> None:
+    print(get_deets([id]))
+
+
 def get_deets(games: list[int]) -> list[Game]:
     appinfo = "https://store.steampowered.com/api/appdetails?appids="
     game_list = []
@@ -69,7 +74,18 @@ def get_deets(games: list[int]) -> list[Game]:
             name = data_dict.get("name", "Unknown")
 
             # Extract freeness
-            price = data_dict.get("price_overview", "unknown")
+            prices = data_dict.get("price_overview", "unknown")
+            if prices != 'unknown':
+                p = prices['final'] / 100
+                p *= exchange_rate if prices['currency'] == 'USD' else 1
+                prices.pop('currency')
+                prices['range'] = 'low' if p <= 10 else 'medium' if p <= 25 else 'high'
+                prices['final'] = p
+                prices.pop('initial')
+                prices.pop('initial_formatted')
+                prices.pop('final_formatted')
+            elif data_dict.get('is_free'):
+                prices = {'final': 0, 'discount_percent': 0, 'range': 'free'}
 
             # Extract lingo
             languages = data_dict.get("supported_languages", "Unknown")
@@ -89,7 +105,7 @@ def get_deets(games: list[int]) -> list[Game]:
             # Extract genres
             genres = [genre_dict['description'] for genre_dict in data_dict.get('genres', [])]
 
-            # Extract description
+            # Extract yap
             description = data_dict.get("short_description", "No description available.")
 
             # Extract system requirements
@@ -97,10 +113,12 @@ def get_deets(games: list[int]) -> list[Game]:
                             'Mac': get_clean_requirements(data_dict.get('mac_requirements')),
                             'Linux': get_clean_requirements(data_dict.get('linux_requirements'))}
 
+            # Extract dlcness
+            dlc = data_dict.get('type', 'unknown') == 'dlc'
             # Append to game list
             game_list.append(
-                Game(game, name, price, description, languages, image, requirements, developers, platforms, categories,
-                     genres))
+                Game(game, name, prices, description, languages, image, requirements, developers, platforms, categories,
+                     genres, dlc))
 
     return game_list
 
@@ -108,9 +126,10 @@ def get_deets(games: list[int]) -> list[Game]:
 def write_to_csv(games: list[Game]) -> None:
     with open("data.csv", "a") as file:
         for game in games:
-            file.write(
-                f"{game.id},'{game.name}',\"{game.price}\",\"[{game.description}]\",\"{game.languages}\",{game.image},\"{game.requirements}\",\"{game.developers}\",\"{game.platforms}\",\"{game.categories}\",\"{game.genres}\"\n")
+            file.write(f"{game.id},'{game.name}',\"{game.price}\",\"[{game.description}]\",\"{game.languages}\","
+                       f"{game.image},\"{game.requirements}\",\"{game.developers}\",\"{game.platforms}\","
+                       f"\"{game.categories}\",\"{game.genres}\",{game.dlc}\n")
 
+write_to_csv(get_deets(get_ids(77, 90)))
 
-
-write_to_csv(get_deets(get_ids(74, 100)))
+#get_single_info(2060270)
--- objects.py
+++ objects.py
@@ -1,11 +1,13 @@
 # Description:
 # Created by Emilia on 2025-02-27
+from typing import Any
+
 
 class Game:
     '''yap herre'''
     id: int
     name: str
-    price: int
+    price: dict[str:Any]
     description: str
     languages: str
     image: str
@@ -14,10 +16,11 @@ class Game:
     platforms: dict[str: bool]
     categories: list[str]
     genres: list[str]
+    dlc: bool
 
     def __init__(self, id: int, name: str, price: int, description: str, languages: str, image: str,
             requirements: dict[str: str], developers: list[str], platforms: dict[str: bool], categories: list[str],
-            genres: list[str]):
+            genres: list[str], dlc):
         self.id = id
         self.name = name
         self.price = price
@@ -29,6 +32,7 @@ class Game:
         self.platforms = platforms
         self.categories = categories
         self.genres = genres
+        self.dlc = dlc
 
     def __repr__(self):
         return f"Game(id={self.id}, name='{self.name}', price={self.price}, genre={self.genres})"
