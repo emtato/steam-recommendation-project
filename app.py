b722be15c9d77f2db3666e479b0dcf8f41285b19
emtato <emiliama2006@gmail.com>
2025-03-25 14:58:54 -0400

    hahahHAHAHAHAHAHAH DHABDAHSd

---
+++ app.py
@@ -0,0 +1,13 @@
+# Description:
+# Created by Emilia on 2025-03-25
+import streamlit as st
+from main import Graph
+
+st.title("QQHHHHHHQHHHQH")
+
+g = Graph()
+g.build_graph("data.csv", 2069)
+
+st.write("Graph built successfully!")
+
+#run streamlit run app.py in terminal
