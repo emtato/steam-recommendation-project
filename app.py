cae54b763de81d5c4aa8b42d5e1f3040e3ee5fe0
emtato <emiliama2006@gmail.com>
2025-03-25 21:44:19 -0400

    If xiaoyue cui has a million fans, then I am one of them. If xiaoyue cui has ten fans, then I am one of them. If xiaoyue cui has only one fan then that is me. If xiaoyue cui has no fans, then that means I am no longer on earth. If the world is against xiaoyue cui, then I am against the world.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 # Description:
- # Description: This python file contains the UI for the project. Here we are using streamlit. You'll need to download
- # the streamlit library and follow the directions in the guide to utilize this.
- # WARNING: for the app to function, please use streamlit version 1.43.2
- # Created by Emilia on 2025-03-25
+# Description: This python file contains the UI for the project. Here we are using streamlit. You'll need to download
+# the streamlit library and follow the directions in the guide to utilize this.
+# WARNING: for the app to function, please use streamlit version 1.43.2
+# Created by Emilia on 2025-03-25
 
 from http.client import responses
 
-
 import streamlit as st
-#from main import Graph
+
+# from main import Graph
 
 results = []
+
 
 def start_page():
     """
@@ -18,20 +19,21 @@ def start_page():
          how the user can use it, and a button that begins the process.
          """
     st.title("Steam game recommendation system!")
-    st.markdown(
-    """
-    Hello user! 👋😎 Are you interested in playing games 🎮🕹️ on Steam but don't really know where to start? 🤔📉 Are you overwhelmed 😵‍💫🌊 by the sheer amount of options 📚💥 on Steam?
+
+    st.markdown("""
+    Hello user! 👋😎 Are you interested in playing games 🎮🕹️ on Steam but don't really know where to start? 🤔📉 
+    Are you overwhelmed 😵‍💫🌊 by the sheer amount of options 📚💥 on Steam?
 
     Well, boy do I have news 📢👀 for you! 🚨🧠
 
     This is the :rainbow[Steam Game Recommendation System] 🌈✨🎮🧃🧠!!
-    Here we'll recommend games 🎲🔥 that YOU 🫵💯 can play/buy 💸🧾 on Steam based on YOUR OWN necessities 🧍‍♀️💅📋! Cool right? 😎😜
+    Here we'll recommend games 🎲🔥 that YOU 🫵💯 can play/buy 💸🧾 on Steam based on YOUR OWN necessities 🧍‍♀️💅📋! 
+    Cool right? 😎😜
 
     (insert some for stuff here / desc 📝💭🛠️)
 
     Click the button below 🔘👇 to get started 🚀🎉🎯
-    """
-    )
+    """)
     if st.button("Start"):
         st.balloons()
         st.session_state["start"] = True
@@ -44,14 +46,8 @@ def pc_req_page():
     1 OPTION TEST
     """
     st.title("Choosing your pc requirements")
-    option_comp = st.selectbox(
-        "What type of computer do you have?",
-        ("PC", "Mac", "Linux"),
-        index=None,
-        placeholder="-",
-    )
-
-
+    option_comp = st.selectbox("What type of computer do you have?", (
+    "PC", "Mac", "Linux"), index=None, placeholder="-", )
 
     # st.write("You selected:", option)
     results.append(option_comp)
@@ -60,63 +56,40 @@ def pc_req_page():
     elif option_comp == "Mac":
         mac_page()
     elif option_comp == "Linux":
-        linux_page()
-    # st.write("You chose: " + results.pop())
-    # game_genre_page()
+        linux_page()  # st.write("You chose: " + results.pop())  # game_genre_page()
 
 
 def pc_page():
     """"""
-    option_OS = st.selectbox(
-        "What Windows OS version do you use?",
-        ("Windows 11", "Windows 10", "Windows 7"),  # May have to replace this with a POSSIBLE OS list from data
-        index=None,
-        placeholder="-",
-    )
-    option_RAM = st.selectbox(
-        "How much RAM does your computer have?",
-        ("8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
-        index=None,
-        placeholder="-",
-    )
-    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")
-    # DONT FORGET TO RESTRICT TO INTEGERS ONLY
+    option_OS = st.selectbox("What Windows OS version do you use?", (
+    "Windows 11", "Windows 10", "Windows 7"),  # May have to replace this with a POSSIBLE OS list from data
+        index=None, placeholder="-", )
+    option_RAM = st.selectbox("How much RAM does your computer have?", (
+    "8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
+        index=None, placeholder="-", )
+    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")  # DONT FORGET TO RESTRICT TO INTEGERS ONLY
 
 
 def mac_page():
     """"""
-    option_OS = st.selectbox(
-        "What Mac OS version do you use?",
-        ("insert"),  # May have to replace this with a POSSIBLE OS list from data
-        index=None,
-        placeholder="-",
-    )
-    option_RAM = st.selectbox(
-        "How much RAM does your computer have?",
-        ("8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
-        index=None,
-        placeholder="-",
-    )
-    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")
-    # DONT FORGET TO RESTRICT TO INTEGERS ONLY
+    option_OS = st.selectbox("What Mac OS version do you use?", (
+        "insert"),  # May have to replace this with a POSSIBLE OS list from data
+        index=None, placeholder="-", )
+    option_RAM = st.selectbox("How much RAM does your computer have?", (
+    "8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
+        index=None, placeholder="-", )
+    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")  # DONT FORGET TO RESTRICT TO INTEGERS ONLY
 
 
 def linux_page():
     """"""
-    option_OS = st.selectbox(
-        "What Linux OS version do you use?",
-        ("isert"),  # May have to replace this with a POSSIBLE OS list from data
-        index=None,
-        placeholder="-",
-    )
-    option_RAM = st.selectbox(
-        "How much RAM does your computer have?",
-        ("8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
-        index=None,
-        placeholder="-",
-    )
-    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")
-    # DONT FORGET TO RESTRICT TO INTEGERS ONLY
+    option_OS = st.selectbox("What Linux OS version do you use?", (
+        "isert"),  # May have to replace this with a POSSIBLE OS list from data
+        index=None, placeholder="-", )
+    option_RAM = st.selectbox("How much RAM does your computer have?", (
+    "8GB", "16GB", "32GB", "32GB+"),  # May have to replace this with a POSSIBLE RAM list from data
+        index=None, placeholder="-", )
+    option_STORAGE = st.text_input("How much storage do you have? (put in GB):")  # DONT FORGET TO RESTRICT TO INTEGERS ONLY
 
 
 def game_genre_page():
@@ -133,27 +106,23 @@ def final_page():
     """
     st.write(str(results))
 
+    # this section checks the session_state and loads the next page, this is to prevent the app's cache from maxing and  # restarting the app, making the user lose progress.
 
-# this section checks the session_state and loads the next page, this is to prevent the app's cache from maxing and
-# restarting the app, making the user lose progress.
+
 if "start" not in st.session_state:
     start_page()
 else:
     pc_req_page()
 
-
-
 # The code below creates tabs! We can use this to show the results later
-    # tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-    # tab1.write("this is tab 1")
-    # tab2.write("this is tab 2")
+# tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+# tab1.write("this is tab 1")
+# tab2.write("this is tab 2")
 
 
+# g = Graph()
+# g.build_graph("data.csv", 1)
 
+# st.write("Graph built successfully!")
 
-#g = Graph()
-#g.build_graph("data.csv", 1)
-
-#st.write("Graph built successfully!")
-
-#run streamlit run app.py in terminal
+# run streamlit run app.py in terminal
